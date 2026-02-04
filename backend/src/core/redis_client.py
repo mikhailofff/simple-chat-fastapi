906d90510e3df8848678953902b7ccc51cd28f71
mikhailofff <asdgrghd@gmail.com>
2026-02-04 21:42:44 +0400

    chore: resolved no-untyped-call mypy errors (#8)

--- backend/src/core/redis_client.py
+++ backend/src/core/redis_client.py
@@ -11,4 +11,4 @@ logger = logging.getLogger(__name__)
 def get_redis_connection() -> Redis:
     redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}"
     logger.debug("Initialized Redis connection")
-    return redis.from_url(url=redis_url, decode_responses=True)
+    return redis.from_url(url=redis_url, decode_responses=True)  # type: ignore[no-untyped-call]
