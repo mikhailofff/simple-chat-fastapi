464328f27ae9adf3399b46d883d1bfa4d972a8e8
mikhailofff <asdgrghd@gmail.com>
2026-02-06 22:18:56 +0400

    fix: restored connection to server

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,8 @@ import logging
 from logging.config import fileConfig
 from os import path
 
+from src.app import app
+
 logging_config_file_path = path.join(path.dirname(path.abspath(__file__)), "logging_config.ini")
 fileConfig(logging_config_file_path, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
@@ -11,4 +13,4 @@ logger.debug("Server module imported; logging configured")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app="server:app", port=8000, reload=True)
+    uvicorn.run(app=app, port=8000, reload=True)
